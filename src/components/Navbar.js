a100516821c7c4cc9b2436a7a69f791a26e7baf0
monsurcodes <monsurcodes@gmail.com>
2025-12-01 18:03:55 +0530

    refactor: remove scroll effect from Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,12 @@
 "use client"
 
-import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 
 const Navbar = () => {
-  const [hasScrolled, setHasScrolled] = useState(false);
-
-  useEffect(() => {
-    const onScroll = () => {
-      setHasScrolled(window.scrollY > 0);
-    };
-    window.addEventListener('scroll', onScroll);
-    return () => window.removeEventListener('scroll', onScroll);
-  }, []);
 
   return (
     <nav
-      className={`flex flex-col sm:flex-row justify-between items-center w-full mx-auto py-5 px-10 sm:px-40 sticky top-0  transition-shadow duration-200 z-50
-        ${hasScrolled ? 'border-b-1 border-slate-700 shadow-lg bg-black' : 'bg-slate-950'}`}
+      className="flex flex-col sm:flex-row justify-between items-center w-full mx-auto py-5 px-10 sm:px-40 sticky top-0  transition-shadow duration-200 z-50 bg-slate-950 shadow-lg backdrop-blur-md shadow-gray-900"
     >
       <div className='text-green-600 text-3xl font-semibold'>
         <Link href="/">monsur.is-a.dev</Link>
